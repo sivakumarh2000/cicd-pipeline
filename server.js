652db945ac323840be6aeaf92f706807d48c0c9b
sivakumarh2000 <sivakumar.h2905@gmail.com>
2024-02-26 13:42:54 +0530

    run

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express')
 
 const app = express()
 
+const users = require("./users/app")
+
+const products = require("./products/app")
+
 app.get('/', (req, res) => {
     res.json({
         status: 200,
         message: "docker-ec2-nginx"
     })
 })
+
+app.use(users, products)
 
 app.listen(8080, () => {
     console.log('server running on port 8080')
